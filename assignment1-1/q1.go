5f934b39a5926068399f55eba862706a06ffe0df
Paul Gallegos <paul.igb@gmail.com>
2020-08-14 20:09:07 -0700

    Convert identations to spaces.

--- assignment1-1/q1.go
+++ assignment1-1/q1.go
@@ -1,61 +1,61 @@
 package cos418_hw1_1
 
 import (
-	"bufio"
-	"fmt"
-	"log"
-	"os"
-	"regexp"
-	"sort"
-	"strings"
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+    "regexp"
+    "sort"
+    "strings"
 )
 
 
 // Remove all special characters of a token using a regular expression and
 // converts all alphabetic characters to lower letters.
-//	r: regular expression used to remove special characters from token.
-// 	word: token to be normalized.
+//  r: regular expression used to remove special characters from token.
+//  word: token to be normalized.
 func normalizeWord(r *regexp.Regexp, word string) string {
-	alphanumericWrd := r.ReplaceAllString(word, "")
-	return strings.ToLower(alphanumericWrd)
+    alphanumericWrd := r.ReplaceAllString(word, "")
+    return strings.ToLower(alphanumericWrd)
 }
 
 
 // Validate if a token qualifies as a word or not.
-// 	word: token to be evaluated.
-//	charThreshold: character threshold for whether a token qualifies as a word,
-//		e.g. charThreshold = 5 means "apple" is a word but "pear" is not.
+//  word: token to be evaluated.
+//  charThreshold: character threshold for whether a token qualifies as a word,
+//      e.g. charThreshold = 5 means "apple" is a word but "pear" is not.
 func validateWord(word string, charThreshold int) bool {
-	return len(word) >= charThreshold
+    return len(word) >= charThreshold
 }
 
 
 // Find the top K most common words in a text document.
-// 	path: location of the document
-//	numWords: number of words to return (i.e. k)
-//	charThreshold: character threshold for whether a token qualifies as a word,
-//		e.g. charThreshold = 5 means "apple" is a word but "pear" is not.
+//  path: location of the document
+//  numWords: number of words to return (i.e. k)
+//  charThreshold: character threshold for whether a token qualifies as a word,
+//      e.g. charThreshold = 5 means "apple" is a word but "pear" is not.
 // Matching is case insensitive, e.g. "Orange" and "orange" is considered the same word.
 // A word comprises alphanumeric characters only. All punctuations and other characters
 // are removed, e.g. "don't" becomes "dont".
 // You should use `checkError` to handle potential errors.
 func topWords(path string, numWords int, charThreshold int) []WordCount {
 
-	// Opening file and logging if there are any errors.
+    // Opening file and logging if there are any errors.
     file, err := os.Open(path)
     if err != nil {
         log.Fatal(err)
     }
-	defer file.Close()
+    defer file.Close()
 
-	scanner := bufio.NewScanner(file)
+    scanner := bufio.NewScanner(file)
     scanner.Split(bufio.ScanWords)
 
     // Hashmap to count word appeareances.
     var wordsCount map[string]int
     wordsCount = make(map[string]int)
 
-	r, _ := regexp.Compile("[^0-9a-zA-Z]+")
+    r, _ := regexp.Compile("[^0-9a-zA-Z]+")
     
     // Normalizing words and counting repetitions.
     // Normalize: Remove special characters to words.
@@ -63,17 +63,17 @@ func topWords(path string, numWords int, charThreshold int) []WordCount {
         word := scanner.Text()
         normalizedWord := normalizeWord(r, word)
         if validateWord(normalizedWord, charThreshold) {
-       		wordsCount[normalizedWord]++
+            wordsCount[normalizedWord]++
         }
     }
 
     // Converting all results into WordCount objects.
     var wcList []WordCount
     for word, count := range wordsCount {
-    	var wc WordCount
-    	wc.Word = word
-    	wc.Count = count
-    	wcList = append(wcList, wc)
+        var wc WordCount
+        wc.Word = word
+        wc.Count = count
+        wcList = append(wcList, wc)
     }
 
     sortWordCounts(wcList)
@@ -83,36 +83,36 @@ func topWords(path string, numWords int, charThreshold int) []WordCount {
     // then all list is returned.
     var result []WordCount
     for i := 0; i < numWords && i < len(wcList); i++ {
-    	result = append(result, wcList[i])
+        result = append(result, wcList[i])
     }
 
     if err := scanner.Err(); err != nil {
         log.Fatal(err)
     }
 
-	return result
+    return result
 }
 
 // A struct that represents how many times a word is observed in a document.
 type WordCount struct {
-	Word  string
-	Count int
+    Word  string
+    Count int
 }
 
 func (wc WordCount) String() string {
-	return fmt.Sprintf("%v: %v", wc.Word, wc.Count)
+    return fmt.Sprintf("%v: %v", wc.Word, wc.Count)
 }
 
 // Helper function to sort a list of word counts in place.
 // This sorts by the count in decreasing order, breaking ties using the word.
 // DO NOT MODIFY THIS FUNCTION!
 func sortWordCounts(wordCounts []WordCount) {
-	sort.Slice(wordCounts, func(i, j int) bool {
-		wc1 := wordCounts[i]
-		wc2 := wordCounts[j]
-		if wc1.Count == wc2.Count {
-			return wc1.Word < wc2.Word
-		}
-		return wc1.Count > wc2.Count
-	})
+    sort.Slice(wordCounts, func(i, j int) bool {
+        wc1 := wordCounts[i]
+        wc2 := wordCounts[j]
+        if wc1.Count == wc2.Count {
+            return wc1.Word < wc2.Word
+        }
+        return wc1.Count > wc2.Count
+    })
 }

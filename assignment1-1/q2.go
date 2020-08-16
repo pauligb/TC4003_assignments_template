799568019ef1e729505d81d0c22be97d1255ad08
Paul Gallegos <paul.igb@gmail.com>
2020-08-15 19:06:36 -0700

    Implement Q2 - Parallel sum
    Creating ´num´ workers and using two channels for the input of
    a list of numbers from a file and the outputs of each worker.
    
    Adding all results and returning the complete sum.

--- assignment1-1/q2.go
+++ assignment1-1/q2.go
@@ -1,17 +1,22 @@
 package cos418_hw1_1
 
 import (
-	"bufio"
-	"io"
-	"strconv"
+    "bufio"
+    "io"
+    "log"
+    "os"
+    "strconv"
 )
 
 // Sum numbers from channel `nums` and output sum to `out`.
 // You should only output to `out` once.
 // Do NOT modify function signature.
 func sumWorker(nums chan int, out chan int) {
-	// TODO: implement me
-	// HINT: use for loop over `nums`
+    sum := 0
+    for num := range nums {
+        sum += num
+    }
+    out <- sum
 }
 
 // Read integers from the file `fileName` and return sum of all values.
@@ -20,10 +25,34 @@ func sumWorker(nums chan int, out chan int) {
 // You should use `checkError` to handle potential errors.
 // Do NOT modify function signature.
 func sum(num int, fileName string) int {
-	// TODO: implement me
-	// HINT: use `readInts` and `sumWorkers`
-	// HINT: used buffered channels for splitting numbers between workers
-	return 0
+    // Opening file and logging if there are any errors.
+    file, err := os.Open(fileName)
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+
+    messages := make(chan int, num)
+    out := make(chan int, num)
+    // Creating `num` workers
+    for i := 0; i < num; i++ {
+        go sumWorker(messages, out)
+    }
+
+    // Reading the file and streaming the information to the producer.
+    integers, err := readInts(file)
+    for _, n := range integers {
+        messages <- n
+    }
+    close(messages)
+
+    // Adding all results to a single result for output.
+    var sum int = 0
+    for i := 0; i < num; i++ {
+        sum += <-out
+    }
+
+    return sum
 }
 
 // Read a list of integers separated by whitespace from `r`.
@@ -31,15 +60,15 @@ func sum(num int, fileName string) int {
 // an empty slice of integers and the error that occurred.
 // Do NOT modify this function.
 func readInts(r io.Reader) ([]int, error) {
-	scanner := bufio.NewScanner(r)
-	scanner.Split(bufio.ScanWords)
-	var elems []int
-	for scanner.Scan() {
-		val, err := strconv.Atoi(scanner.Text())
-		if err != nil {
-			return elems, err
-		}
-		elems = append(elems, val)
-	}
-	return elems, nil
+    scanner := bufio.NewScanner(r)
+    scanner.Split(bufio.ScanWords)
+    var elems []int
+    for scanner.Scan() {
+        val, err := strconv.Atoi(scanner.Text())
+        if err != nil {
+            return elems, err
+        }
+        elems = append(elems, val)
+    }
+    return elems, nil
 }
